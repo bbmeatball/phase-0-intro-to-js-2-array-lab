0c678f481cda8d16fc7aa731f78af63c16567132
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-02-11 15:28:37 -0700

    completed assigment

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 // Write your solution here!
-test
-test
+const cats=["Milo","Otis","Garfield"];
+cats.length=0;
+cats.push('Milo','Otis','Garfield');
+
+function destructivelyAppendCat(name){
+ cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}   
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name){
+    return [...cats,name];
+}
+function prependCat(name){
+    return [name, ...cats];
+}
+
+function removeLastCat(name){
+ return cats.slice(0,cats.length-1);
+}
+
+function removeFirstCat(name){
+    return cats.slice(1);
+}
